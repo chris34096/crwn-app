27588fac4f0f6c45504d2c0781baee4483817de2
Christofer Perez <christoferperez@gmail.com>
2019-10-04 15:30:38 +0200

    adding docs

---
+++ docs/precache-manifest.62368e7f55a0dc54b6728f24bb0c4c08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93722855d9b06908836af94efd1274c7",
+    "url": "/crwn-app/index.html"
+  },
+  {
+    "revision": "b5ada685fc4b86d1d3ed",
+    "url": "/crwn-app/static/css/main.e4f23c35.chunk.css"
+  },
+  {
+    "revision": "66e91a05a655f34d37da",
+    "url": "/crwn-app/static/js/2.20750105.chunk.js"
+  },
+  {
+    "revision": "b5ada685fc4b86d1d3ed",
+    "url": "/crwn-app/static/js/main.afc64793.chunk.js"
+  },
+  {
+    "revision": "665ecd18e94f62d0723a",
+    "url": "/crwn-app/static/js/runtime~main.2d3eba1b.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/crwn-app/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/crwn-app/static/media/shopping-bag.30f07071.svg"
+  }
+]);
