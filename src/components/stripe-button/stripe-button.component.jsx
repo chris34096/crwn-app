dfac2e128559947c916302a70ae47c6781be196a
Christofer Perez <christoferperez@gmail.com>
2019-10-10 13:24:53 +0200

    adding icon

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = "pk_test_YO4WxsR84ywdhnMZG3sVk3N400xyP868jP";
+
+  const onToken = token => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="E-commerce Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is ${price}€`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      currency="EUR"
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+import StripeCheckoutButton from "../../components/stripe-button/stripe-button.component";
 
 import {
   selectCartItems,
@@ -9,6 +10,7 @@ import {
 } from "../../redux/cart/cart.selector";
 
 import "./checkout.styles.scss";
+import StripeCheckout from "../../components/stripe-button/stripe-button.component";
 
 const CheckoutPage = ({ cartItems, total }) => (
   <div className="checkout-page">
@@ -35,6 +37,12 @@ const CheckoutPage = ({ cartItems, total }) => (
 
     <div className="total">
       <span>TOTAL: ${total}</span>
+      <div className="test-warning">
+        *Please use the following test credit card for payments*
+        <br />
+        4242 4242 4242 4242 - Exp:01/20 - CVV: 123
+      </div>
+      <StripeCheckoutButton price={total} />
     </div>
   </div>
 );
